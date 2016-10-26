c5182de1e896d9de31f502463d0beced99674f61
BilboBaggins6704 <lukethomasdell123@gmail.com>
2016-10-26 15:42:18 +0100

    Removed the sound of button download.

--- lua/autorun/server/gescape.lua
+++ lua/autorun/server/gescape.lua
@@ -1,5 +1,2 @@
-AddCSLuaFile("autorun/client/gescape.lua")
-
 resource.AddFile("sound/gescape/open.mp3")
-resource.AddFile("sound/gescape/button.mp3")
 resource.AddFile("materials/gescape/options.png")
